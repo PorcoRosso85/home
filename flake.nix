0847fd82be26e2ee66126c8cd6541450a52fdf60
PorcoRosso85 <1.is.universe@gmail.com>
2024-10-05 06:59:27 +0900

    update home

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My Nix Home Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # 適切なNixpkgsのバージョンを指定
+    home-manager.url = "github:nix-community/home-manager"; # or your desired branch
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    homeConfigurations.rocchoHome = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      # extraSpecialArgs = { inherit inputs; };
+      modules = [
+        ./home.nix
+        {
+          home = {
+            username = "user"; # あなたのユーザー名
+            homeDirectory = "/home/user"; # あなたのホームディレクトリ
+            stateVersion = "23.11"; # 適切なバージョンに更新
+          };
+        }
+      ];
+    };
+  };
+}
