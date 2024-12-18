d335fa24afd0cf0ae081d044bcc9c9695ee5fc10
Tetsuya Takasawa <40359643+PorcoRosso85@users.noreply.github.com>
2024-12-18 19:19:52 +0900

    update home

--- nix/home.nix
+++ nix/home.nix
@@ -8,21 +8,21 @@ in
   programs = {
     home-manager.enable = true;
 
-    bash = {
-      enable = true;
-      initExtra = ''
-        export TERM="xterm-256color"
-        export COLORTERM="truecolor"
-        eval "$(dircolors -b)"
-        source "$HOME/.cargo/env" # 必要に応じて
-      '';
-      shellAliases = {
-        ll = "ls -alF";
-        la = "ls -A";
-        l = "ls -CF";
-        # その他のエイリアス
-      };
-    };
+    # bash = {
+    #   enable = true;
+    #   initExtra = ''
+    #     export TERM="xterm-256color"
+    #     export COLORTERM="truecolor"
+    #     eval "$(dircolors -b)"
+    #     source "$HOME/.cargo/env" # 必要に応じて
+    #   '';
+    #   shellAliases = {
+    #     ll = "ls -alF";
+    #     la = "ls -A";
+    #     l = "ls -CF";
+    #     # その他のエイリアス
+    #   };
+    # };
 
     dircolors = { enable = true; }; # カスタムカラーが必要ならここで設定
 
