b5885ecfc0e537d2b75525cf0bba6227c3373a22
PorcoRosso85 <1.is.universe@gmail.com>
2025-04-25 23:36:14 +0000

    kuzu, local

--- bin/src/kuzu/init.ts
+++ bin/src/kuzu/init.ts
@@ -1,4 +1,9 @@
 #!/usr/bin/env -S nix run nixpkgs#deno -- run --allow-all --allow-scripts=npm:kuzu@0.9.0
+
+/**
+❯ LD_LIBRARY_PATH=/nix/store/2y8c3b7ydkl68liz336035llfhmm6r95-gfortran-14-20241116-lib/lib/:$LD_LIBRARY_PATH ./init.ts
+*/
+
 import * as kuzu from "kuzu";
 
 (async () => {
