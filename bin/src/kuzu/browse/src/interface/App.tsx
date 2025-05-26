8faa7ddb9057f773530bdf09fbcbbc4cd708f67d
PorcoRosso85 <1.is.universe@gmail.com>
2025-05-26 11:21:35 +0000

    rpc, kuzu

--- bin/src/kuzu/browse/src/interface/App.tsx
+++ bin/src/kuzu/browse/src/interface/App.tsx
@@ -9,9 +9,32 @@ import { Page } from './Page';
  * アプリケーションのメインコンポーネント
  */
 const App: React.FC = () => {
+  const handleLsClick = () => {
+    const ws = new WebSocket("ws://localhost:8080");
+    
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        jsonrpc: "2.0",
+        method: "ls",
+        params: { args: ["-la"] },
+        id: 1
+      }));
+    };
+    
+    ws.onmessage = (e) => {
+      const res = JSON.parse(e.data);
+      console.log(res.result?.stdout || res.error);
+      alert(res.result?.stdout || JSON.stringify(res.error));
+      ws.close();
+    };
+  };
+
   return (
     <Layout>
       <Page />
+      <button onClick={handleLsClick} style={{position: 'fixed', bottom: 20, right: 20}}>
+        LS実行
+      </button>
     </Layout>
   );
 };
