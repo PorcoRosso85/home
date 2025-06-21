224b6ebc00201e6f859311e6fedb320ba6d4509b
PorcoRosso85 <1.is.universe@gmail.com>
2025-06-21 04:51:04 +0000

    tmux, add, window protection

--- tmux_minimal.sh
+++ tmux_minimal.sh
@@ -23,6 +23,9 @@ tmux bind-key -n M-3 select-window -t 3
 tmux set -g window-style 'fg=colour245'
 tmux set -g window-active-style 'fg=colour255'
 
+# ペイン保護設定
+tmux set -g remain-on-exit on
+
 # 最初のウィンドウを左右分割
 tmux split-window -h -t $SESSION_NAME:editor
 
@@ -33,6 +36,14 @@ tmux split-window -h -t $SESSION_NAME:terminal
 # 3つ目のウィンドウを作成（logs）
 tmux new-window -t $SESSION_NAME -n "logs"
 tmux split-window -h -t $SESSION_NAME:logs
+
+# 各ペインで保護シェルを起動
+for window in editor terminal logs; do
+    for pane in 0 1; do
+        tmux send-keys -t $SESSION_NAME:$window.$pane "trap '' INT TERM EXIT; set -o ignoreeof; IGNOREEOF=1000" Enter
+        tmux send-keys -t $SESSION_NAME:$window.$pane "echo 'Protected pane - Use tmux kill-session to exit'" Enter
+    done
+done
 
 # 最初のウィンドウに戻る
 tmux select-window -t $SESSION_NAME:editor
