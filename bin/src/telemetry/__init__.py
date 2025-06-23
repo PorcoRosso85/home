8e95aeb8ccf680b2cf308f984ac1f4eda9673a8b
PorcoRosso85 <1.is.universe@gmail.com>
2025-06-23 22:20:16 +0000

    telemetry, refactor

--- bin/src/telemetry/__init__.py
+++ bin/src/telemetry/__init__.py
@@ -1,1 +1,10 @@
-# Telemetry module initialization
+"""
+Telemetry module - Logging and observability infrastructure
+
+Primary export:
+- log: Main logging function
+"""
+
+from .application.logging import log
+
+__all__ = ['log']
