27c21037b9bf9e22cb46a75b4942b0871beefd13
PorcoRosso85 <1.is.universe@gmail.com>
2025-07-10 07:11:38 +0000

    feat: lsp lsmcp

--- bin/src/poc/develop/lsp/lsmcp/examples/go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/mizchi/lsmcp/examples/go
-
-go 1.21
--- bin/src/poc/develop/lsp/lsmcp/tests/adapters/fixtures/typescript/simple.ts
+++
@@ -1,8 +0,0 @@
-// Simple file to test tsgo diagnostics
-const x: number = "hello"; // Type error
-
-function test() {
-  return 42;
-}
-
-const result: string = test(); // Type error
