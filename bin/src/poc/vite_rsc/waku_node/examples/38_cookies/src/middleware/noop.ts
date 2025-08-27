ce8aad3aef992f784fa05b1c5d5795427b03d9f9
PorcoRosso85 <1.is.universe@gmail.com>
2025-08-27 19:48:02 +0900

    feat: poc waku base

--- bin/src/poc/vite_rsc/waku_node/examples/38_cookies/src/middleware/noop.ts
+++
@@ -1,9 +0,0 @@
-import type { Middleware } from 'waku/config';
-
-const noopMiddleware: Middleware = () => {
-  return async (_ctx, next) => {
-    await next();
-  };
-};
-
-export default noopMiddleware;
